4c11981b405feed76c408791a12c4e247f158734
Tim Davies <timdavies@me.com>
2019-09-27 09:46:52 +0100

    Initial Plugin Content

---
+++ Contents/Sketch/script.js
@@ -0,0 +1,34 @@
+const Document = require('sketch/dom').Document
+const UI = require('sketch/ui')
+
+function copyObjectUUID(context) {
+  const selectedLayers = Document.getSelectedDocument().selectedLayers
+  const layerIDs = selectedLayers.map(layer => {
+    return layer.id
+  })
+
+  copyToClipboard(layerIDs.join(", "))
+
+  UI.message(`Copied ${layerIDs.length} Artboard or Layer UUID(s) 🎉`)
+}
+          
+function copyPageUUID(context) {
+  const page = Document.getSelectedDocument().selectedPage
+
+  copyToClipboard(page.id)
+  UI.message(`Copied UUID for page "${page.name}" 🎉`)
+}
+
+function copyDocumentUUID(context) {
+  const doc = Document.getSelectedDocument()
+
+  copyToClipboard(doc.id)
+  UI.message(`Copied document UUID 🎉`)
+}
+         
+          
+const copyToClipboard = (value) => {
+  const pasteboard = NSPasteboard.generalPasteboard()
+  pasteboard.declareTypes_owner( [ NSPasteboardTypeString ], null );
+	pasteboard.setString_forType( value, NSPasteboardTypeString );
+}
