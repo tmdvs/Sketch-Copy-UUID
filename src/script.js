e2b7015315772ab6752a74b3faa210af58ae3ee9
Tim Davies <timdavies@me.com>
2019-11-14 10:16:46 +0000

    Update commands to be more obvious

--- src/script.js
+++ src/script.js
@@ -1,34 +1,56 @@
 const Document = require('sketch/dom').Document
 const UI = require('sketch/ui')
 
-export function copyObjectUUID(context) {
-  const selectedLayers = Document.getSelectedDocument().selectedLayers
-  const layerIDs = selectedLayers.map(layer => {
-    return layer.id
-  })
+// Copy selected artboard identifiers
+export function copyArtboardUUIDs(context) {
+  const selected = Document.getSelectedDocument().selectedLayers
+  const artboardIds = selected.layers
+    .filter(layer => layer.type =="Artboard")
+    .map(layer => layer.id)
 
-  copyToClipboard(layerIDs.join(", "))
-
-  UI.message(`Copied ${layerIDs.length} Artboard or Layer UUID(s) 🎉`)
+  if(artboardIds.length > 0) {
+    copyToClipboard(artboardIds, "artboard")
+  } else {
+    UI.message(`It looks like you haven't selected any artboards!`)
+  }
 }
-          
+
+// Copy selected layers identifiers
+export function copyLayerUUIDs(context) {
+  const selected = Document.getSelectedDocument().selectedLayers
+  const layerIds = selected.layers
+    .filter(layer => layer.type !="Artboard")
+    .map(layer => layer.id)
+
+  if(layerIds.length > 0) {
+    copyToClipboard(layerIds, "layer")
+  } else {
+    UI.message(`It looks like you haven't selected any layers!`)
+  }
+}
+         
+// Copy current page identifier
 export function copyPageUUID(context) {
   const page = Document.getSelectedDocument().selectedPage
-
-  copyToClipboard(page.id)
-  UI.message(`Copied UUID for page "${page.name}" 🎉`)
+  copyToClipboard([page.id], "current page")
 }
 
+// Copy document identifier
 export function copyDocumentUUID(context) {
   const doc = Document.getSelectedDocument()
-
-  copyToClipboard(doc.id)
-  UI.message(`Copied document UUID 🎉`)
+  copyToClipboard([doc.id], "current document")
 }
          
           
-const copyToClipboard = (value) => {
+const copyToClipboard = (ids = [], type = "") => {
   const pasteboard = NSPasteboard.generalPasteboard()
   pasteboard.declareTypes_owner( [ NSPasteboardTypeString ], null );
-	pasteboard.setString_forType( value, NSPasteboardTypeString );
+  pasteboard.setString_forType( ids.join(", "), NSPasteboardTypeString );
+
+  if(ids.length == 1) {
+    UI.message(`📎  Copied ${type} UUID`)
+  } else {
+    UI.message(`📎  Copied ${ids.length} ${type} UUIDs`)
+  }
+
 }
